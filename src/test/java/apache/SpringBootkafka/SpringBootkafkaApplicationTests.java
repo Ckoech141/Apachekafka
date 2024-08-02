b81b3c812590d283bba689077840e8f3b5434509
ckoech141 <ckoech141@gmail.com>
2024-08-02 16:04:59 +0300

    Added Kafka dependecies

---
+++ src/test/java/apache/SpringBootkafka/SpringBootkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package apache.SpringBootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootkafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
